c653ea7d279e209b78a75221b411603b5e493526
rushabh <rushabhpawar01@gamil.com>
2023-03-23 11:34:52 +0530

    rushabh

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,19 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from  Rushabh");
+
+
+       double b=15.45;
+
+       int a = (int)b;
+       System.out.println(b);
+
+
+
+
     }
+
+
+
 
 }
