fe8b1fb2403b21af4c44a9a47c98de1b5428f786
rushabh <rushabhpawar01@gamil.com>
2023-03-23 14:50:01 +0530

    rsp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from  Rushabh");
+        System.out.println("Hello  Rushabh");
 
 
        double b=15.45;
